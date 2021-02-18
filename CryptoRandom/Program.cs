04f70e1c252d335424df784f9b15dc7b7c7f6536
Leighzer <bwc_nothgiel@live.com>
2021-02-17 23:41:25 -0500

    initial commit;

---
+++ CryptoRandom/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CryptoRandom
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CryptoRandom random = new CryptoRandom();
+            Console.WriteLine($"Hello {random.Next().ToString()}!");
+        }
+    }
+}
